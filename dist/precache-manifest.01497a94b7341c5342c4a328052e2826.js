028839c786f7c72b5026b7c680ae3b612a0efd6f
kamicauze <nkamau417@gmail.com>
2019-01-06 21:45:00 +0300

    social buttons

---
+++ dist/precache-manifest.01497a94b7341c5342c4a328052e2826.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "0937500d37b67ef53848",
+    "url": "/js/about.ec1e39f7.js"
+  },
+  {
+    "revision": "2f7a9223395727bf3d06",
+    "url": "/css/app.b36e3be1.css"
+  },
+  {
+    "revision": "2f7a9223395727bf3d06",
+    "url": "/js/app.a36b0fdc.js"
+  },
+  {
+    "revision": "dccc95ad035b8a485e63",
+    "url": "/js/chunk-vendors.b27494bf.js"
+  },
+  {
+    "revision": "a3952587f27d4a99f33155b012bfc9e4",
+    "url": "/img/skull-headphones-wallpaper-58695-60469-hd-wallpapers.a3952587.jpg"
+  },
+  {
+    "revision": "8a0ddf2f3f5fefc6c1a0069a4f09b1e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35ecd210b55c5fd890d0ee7081f2e525",
+    "url": "/img/east.webp"
+  },
+  {
+    "revision": "dd7658bf5a75232715216203c5589b36",
+    "url": "/img/kameme.png"
+  },
+  {
+    "revision": "f1b6f035dce91bb01e017d6262138ab2",
+    "url": "/img/woman.jpeg"
+  },
+  {
+    "revision": "7796855db31ed8262d8366280954170d",
+    "url": "/img/capital.jpg"
+  },
+  {
+    "revision": "3b775b8bc8629f948863fb39250e858c",
+    "url": "/img/classic.jpg"
+  },
+  {
+    "revision": "5e725eaa0624448a35d738226785f4d7",
+    "url": "/img/ghetto.jpg"
+  },
+  {
+    "revision": "7118da1b6978def044fce1d8060f5a16",
+    "url": "/img/hope.jpg"
+  },
+  {
+    "revision": "227b26eeaf6af88ebcfa6812bf815312",
+    "url": "/img/east.jpg"
+  },
+  {
+    "revision": "6d8fab2e88af83dc5b931bcf33b59ce3",
+    "url": "/img/radiomaisha.jpg"
+  },
+  {
+    "revision": "519993223b5d1418e6122c23fcf001f6",
+    "url": "/img/voa.jpg"
+  },
+  {
+    "revision": "73659ea263a2cc12c4ed92fad802543d",
+    "url": "/img/hbr.jpg"
+  },
+  {
+    "revision": "2af79f314c29e17cef5291ad6ec0fc48",
+    "url": "/img/inooro.webp"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "4a8953c0ba313f87758faab63dfb25a9",
+    "url": "/img/xfm.jpg"
+  },
+  {
+    "revision": "6b5998349c6e012246d8841a74185bcb",
+    "url": "/img/wonan/wonan3.jpg"
+  },
+  {
+    "revision": "a0d82fdb0336740cbf14be89deb3d789",
+    "url": "/img/milele.jpg"
+  },
+  {
+    "revision": "716e2a9b24a3af50a6ad94a04944564e",
+    "url": "/img/kiss.jpg"
+  },
+  {
+    "revision": "d569ec0dfa3c9f94d2cf87ea2b6c2e0e",
+    "url": "/img/radiojambo.jpg"
+  },
+  {
+    "revision": "e8bd8cd6d3318e67378f1a648289ecd6",
+    "url": "/img/nrg.webp"
+  },
+  {
+    "revision": "9b30b2fefca1792b5bee4000cc9be9ea",
+    "url": "/img/load.gif"
+  },
+  {
+    "revision": "6cc831d32293d85f7c1beeffc94df0cf",
+    "url": "/img/wonan/wonan2.jpg"
+  },
+  {
+    "revision": "d4666a8bc59a94c6f8429caa65149ac0",
+    "url": "/img/waumini.jpg"
+  },
+  {
+    "revision": "18187253c93d95a23091cb251c801ea1",
+    "url": "/img/wonan/wonan5.jpg"
+  },
+  {
+    "revision": "32460f03226c11d308ab4f6e6366f743",
+    "url": "/img/wonan/wonan0.jpg"
+  },
+  {
+    "revision": "32460f03226c11d308ab4f6e6366f743",
+    "url": "/img/031.jpg"
+  },
+  {
+    "revision": "c49e4981217d43d59a4b6cf8393740f6",
+    "url": "/img/wonan/wonan.jpg"
+  },
+  {
+    "revision": "68c98e9b5b03e9537a18eb21a551d00b",
+    "url": "/img/viz.gif"
+  },
+  {
+    "revision": "22fb7fbcf16d0409e2e89d4d1388a40d",
+    "url": "/img/audio.gif"
+  }
+];
